6f0b5f669fcf2e4f274dc4b6745699a9ae1c5be4
Steve Guardado <guas5373@gmail.com>
2017-10-12 20:11:34 +0000

    update flashes

--- routes/index.js
+++ routes/index.js
@@ -35,7 +35,7 @@ router.post("/register", function(req, res)
         //redirect to campgrounds upon successful authentication
         passport.authenticate("local")(req, res, function()
         {
-            req.flash("success", "Welcome to YelpCamp " + user.username);
+            req.flash("success", "Welcome to ParkHop " + user.username);
            res.redirect("/campgrounds"); 
         });
     });
